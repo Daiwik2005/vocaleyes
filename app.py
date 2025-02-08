0758eb9bf41349a91d1c01d117b023ed323800d6
Daiwik2005 <daiwikpalavarapu755@gmail.com>
2025-02-08 20:43:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if not api_key:
 
 genai.configure(api_key=api_key)
 
-    model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 else:
     st.error("⚠️ API Key not found! Please check Streamlit Secrets.")
     st.stop()
